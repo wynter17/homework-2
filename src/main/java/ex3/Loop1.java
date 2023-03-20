f192fc31248a71104d9003c532689848ff57f2dc
david <redx1794@gmail.com>
2023-03-20 20:30:46 +0200

    ex3

---
+++ src/main/java/ex3/Loop1.java
@@ -0,0 +1,16 @@
+package ex3;
+import java.io.*;
+
+
+
+public class Loop1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i = 1;i <= 10;i++){
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+        bufferedReader.close();
+    }
+}
