b737b9501bd22791352dcf9a9a8c566a265cfbbe
david <redx1794@gmail.com>
2023-03-20 20:27:37 +0200

    ex2

--- src/main/java/ex2/Scanner2.java
+++ src/main/java/ex2/Scanner2.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 
 public class Scanner2 {
 
+//    https://www.hackerrank.com/challenges/java-stdin-stdout/problem?isFullScreen=true
+
 
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             int i = scan.nextInt();
             double d = scan.nextDouble();
+            scan.nextLine();
             String s = scan.nextLine();
             scan.close();
 
